4b2266b108fb29e9794430f01e1b61821c0a8ed9
yqbk <jksyrek@gmail.com>
2019-04-24 21:19:58 +0200

    Add dome components

--- screens/Lesson.tsx
+++ screens/Lesson.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+// import { Font } from 'expo';
+// import { Ionicons } from '@expo/vector-icons';
+import {
+  Container,
+  Header,
+  Content,
+  Accordion,
+  Button,
+  Text,
+  Icon,
+  List,
+  ListItem,
+  Body,
+  Title,
+} from 'native-base';
+
+import { ListView } from 'react-native';
 
 import { connect } from 'react-redux';
 import { lifecycle, compose, withProps, pure } from 'recompose';
 
-import { Text, Button, View, FlatList } from 'react-native';
+import { View, FlatList } from 'react-native';
 import styled from 'styled-components';
 
 import LessonsActions from '../state/lessonsReducer';
@@ -11,49 +28,61 @@ import CardsActions from '../containers/cards/actions';
 import { bindActionCreators } from '../utils/reduxUtils';
 import { cardsSelector } from '../containers/cards/selector';
 
+const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+
 const LessonScreen = ({ navigation, removeLesson, cards, getCards, addCard }) => {
   const { title, id, ...rest } = navigation.getParam('lesson');
 
   console.log('cards', cards);
 
   return (
-    <LessonView>
-      <View style={{ flex: 1, flexDirection: 'column', borderWidth: 2, paddingBottom: 32 }}>
-        <Text> {title} </Text>
-        <Button title="get cards" onPress={() => getCards(id)} />
-        <Button title="add question" onPress={() => addCard(id, 'pytanie', 'odpowiedz')} />
-      </View>
-
-      <CardsView>
-        {cards && (
-          <FlatList
-            data={[{ isNew: true }, ...cards]}
-            keyExtractor={this._keyExtractor}
-            renderItem={({ item }) => (
-              <View>
-                <Text>
-                  Card -> {item.ask} = {item.answer}
-                </Text>
-              </View>
-            )}
-          />
-        )}
-      </CardsView>
-      <Button color="red" title=" Delete lesson " onPress={() => removeLesson(id)} />
-    </LessonView>
+    // <LessonView>
+    <Container>
+      <Content padder contentContainerStyle={{ flexGrow: 1 }}>
+        <List
+          leftOpenValue={75}
+          rightOpenValue={-75}
+          dataSource={ds.cloneWithRows(cards)}
+          renderRow={data => (
+            <ListItem>
+              <Text> {data.ask} </Text>
+              <AnswerText> {data.answer} </AnswerText>
+            </ListItem>
+          )}
+          renderLeftHiddenRow={data => (
+            <Button full onPress={() => alert(data)}>
+              <Icon active name="information-circle" />
+            </Button>
+          )}
+          renderRightHiddenRow={(data, secId, rowId, rowMap) => (
+            <Button full danger onPress={_ => alert('delete')}>
+              <Icon active name="trash" />
+            </Button>
+          )}
+        />
+      </Content>
+      <Button danger big onPress={() => removeLesson(id)}>
+        <Text> Delete lesson </Text>
+      </Button>
+    </Container>
+    // </LessonView>
   );
 };
 
 const CardsView = styled.View`
-  border: 1px solid red;
   flex: 1;
 `;
 
 const LessonView = styled.View`
   border: 1px solid #000000ab;
-  border-radius: 5px;
   flex: 1;
   padding-top: 15;
+  justify-content: center;
+`;
+
+const AnswerText = styled.Text`
+  font-size: 12px;
+  color: gray;
 `;
 
 const mapStateToProps = state => ({
@@ -71,12 +100,7 @@ export default compose(
     mapStateToProps,
     mapDispatchToProps
   ),
-  // withProps(({ closeDialog }) => ({
-  // TODO SELECTOR?
-  //   timeout: () => {
-  //     closeDialog();
-  //   },
-  // })),
+
   lifecycle({
     componentDidMount() {
       const { title, id, ...rest } = this.props.navigation.getParam('lesson');
