b93d9d1464b333520f585d9fec3753b58cd0b13d
yqbk <jksyrek@gmail.com>
2019-04-27 20:53:45 +0200

    Dispaly cards!

--- screens/Home/Quiz/QuizScreen.tsx
+++ screens/Home/Quiz/QuizScreen.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 import {
   Container,
   Content,
-  Button,
   Text,
-  Icon,
-  List,
-  ListItem,
+  DeckSwiper,
+  Card,
+  CardItem,
 } from 'native-base'
 
 import { ListView, Alert } from 'react-native'
@@ -19,6 +18,7 @@ import styled from 'styled-components'
 
 import { bindActionCreators } from '../../../utils/reduxUtils'
 import { cardsSelector } from '../../../containers/cards/selector'
+import FlipCard from 'react-native-flip-card'
 
 const QuizScreen = ({
   // navigation,
@@ -27,22 +27,86 @@ const QuizScreen = ({
   getCards,
   removeCard,
 }) => {
+  console.log('->', cards)
   return (
     <Container>
-      <Content padder contentContainerStyle={{ flexGrow: 1 }}>
-        <Text>Quiz</Text>
+      <Content
+        padder
+        contentContainerStyle={{
+          flexGrow: 1,
+        }}
+      >
+        <DeckSwiper
+          dataSource={cards}
+          renderItem={item => (
+            <Card
+              style={{
+                flex: 1,
+                height: 500,
+              }}
+            >
+              <CardItem
+                cardBody
+                style={{
+                  flex: 1,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <FlipCard
+                  style={{
+                    flex: 1,
+                    borderColor: 'transparent',
+                    width: 350,
+                    elevation: 3,
+                  }}
+                  friction={6}
+                  perspective={1000}
+                  flipVertical
+                  flip={false}
+                  clickable={true}
+                  onFlipEnd={isFlipEnd => {
+                    console.log('isFlipEnd', isFlipEnd)
+                  }}
+                >
+                  <CardFace>
+                    <Text>{item.ask}</Text>
+                  </CardFace>
+
+                  <CardBack>
+                    <Text>{item.answer}</Text>
+                  </CardBack>
+                </FlipCard>
+              </CardItem>
+            </Card>
+          )}
+        />
       </Content>
     </Container>
   )
 }
+
+const CardFace = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  border: 3px solid blue;
+  border-radius: 3px;
+`
+
+const CardBack = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  border: 3px solid red;
+  border-radius: 3px;
+`
 
 const mapStateToProps = state => ({
   cards: cardsSelector(state),
 })
 
 const mapDispatchToProps = bindActionCreators({
-  // removeLesson: title => LessonsActions.removeLessonRequest(title),
-  // getCards: lessonID => CardsActions.getCardsRequest(lessonID),
   // removeCard: cardId => CardsActions.removeCardRequest(cardId),
 })
 
