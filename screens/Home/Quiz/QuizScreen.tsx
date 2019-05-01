12563907b3ec6da40f48701e4d6cc0eeca6aa74f
yqbk <jksyrek@gmail.com>
2019-05-01 22:28:34 +0200

    Cleanup Quiz component.

--- screens/Home/Quiz/QuizScreen.tsx
+++ screens/Home/Quiz/QuizScreen.tsx
@@ -1,29 +1,11 @@
 import React from 'react'
 import { Bar } from 'react-native-progress'
-import {
-  Container,
-  Content,
-  Text,
-  DeckSwiper,
-  Card,
-  CardItem,
-  Button,
-  Icon,
-} from 'native-base'
+import { Container, Text, Card, CardItem } from 'native-base'
 
-import Swiper from 'react-native-deck-swiper'
 import SwipeCards from 'react-native-swipe-cards'
 
-import { ListView, Alert } from 'react-native'
-
 import { connect } from 'react-redux'
-import {
-  lifecycle,
-  compose,
-  withState,
-  withHandlers,
-  withPropsOnChange,
-} from 'recompose'
+import { lifecycle, compose, withState, withHandlers } from 'recompose'
 
 import { View } from 'react-native'
 import styled from 'styled-components'
@@ -32,11 +14,7 @@ import { bindActionCreators } from '../../../utils/reduxUtils'
 import { cardsSelector } from '../../../containers/cards/selector'
 import FlipCard from 'react-native-flip-card'
 
-Array.prototype.insert = function(index: number, item: Object): void {
-  this.splice(index, 0, item)
-}
-
-function shuffle(a) {
+const shuffle = (a: Array<Object>) => {
   for (let i = a.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
     ;[a[i], a[j]] = [a[j], a[i]]
@@ -44,94 +22,41 @@ function shuffle(a) {
   return a
 }
 
-const QuizScreen = ({
-  // navigation,
-  removeLesson,
-  cards,
-  cardsList,
-  setCards,
-  getCards,
-  removeCard,
-  changeProgress,
-}) => {
+const QuizScreen = ({ cards, cardsList, setCards }) => {
   const handleSwipeLeft = item => {
-    console.log('item', item)
-    const cardsWithoutItem = cards.filter(card => card.id !== item.id)
-    console.log(
-      ' -> ',
-      item.id,
-      cardsWithoutItem.map(card => card.ask),
-      cards.map(card => card.ask),
-    )
-    setCards(cardsWithoutItem)
-    // console.log('=>', this._deckSwiper._root.props.dataSource.length)
+    setCards(shuffle([...cards]))
   }
 
-  // repeat car
   const handleSwipeRight = item => {
-    const newIndex = Math.floor(Math.random() * (cards.length - 1))
-    const lessCards = cards.slice(1)
-    const firstItem = cards[0]
-    const shuffled = shuffle(cards.slice(1))
-    const cardsWithRepeatedItem = [firstItem, ...shuffled]
-    // debugger
-    setCards(cardsWithRepeatedItem)
-    console.log(
-      ' -> ',
-      cardsWithRepeatedItem.map(card => card.ask),
-      cards.map(card => card.ask),
-    )
-    // console.log('==', this._deckSwiper._root)
-    // console.log('=>', this._deckSwiper._root.props.dataSource.length)
-    // changeProgress()
+    const cardsWithoutItem = cards.filter(card => card.id !== item.id)
+    setCards(cardsWithoutItem)
   }
-
-  // const handleYup = card => {
-  //   console.log(`Yup for ${card.text}`)
-
-  //   this.setState({
-  //     completed: this.state.completed + 1,
-  //   })
-  // }
-  // const handleNope = card => {
-  //   const shuffled = shuffle([...this.state.cards])
-
-  //   console.log(shuffled[0])
-
-  //   this.setState({
-  //     cards: shuffled,
-  //   })
-
-  //   console.log(`Nope for ${card.text}`)
-  // }
-  // const handleMaybe = card => {
-  //   this.setState({
-  //     cards: [...this.state.cards, card],
-  //   })
-
-  //   console.log(`Maybe for ${card.text}`)
-  // }
 
   return (
     <Container>
       {Array.isArray(cards) && (
         <SwipeCards
           cards={cards}
+          // containerStyle={{ padding: 62, borderWidth: 5, borderColor: 'red' }}
+          yupText="Ok"
+          noText="Repeat"
           renderCard={item => (
             <Card
               style={{
-                flex: 1,
+                // flex: 1,
                 height: 300,
+                width: 300,
+                // margin: 64,
                 //
-                borderWidth: 1,
-                borderRadius: 2,
-                borderColor: '#ddd',
-                borderBottomWidth: 0,
-                shadowColor: '#000',
-                shadowOffset: { width: 0, height: 2 },
-                shadowOpacity: 0.8,
-                shadowRadius: 2,
-                elevation: 1,
+                // borderWidth: 1,
+                // borderRadius: 2,
+                // borderColor: '#ddd',
+                // borderBottomWidth: 0,
+                // shadowColor: '#000',
+                // shadowOffset: { width: 0, height: 2 },
+                // shadowOpacity: 0.8,
+                // shadowRadius: 2,
+                // elevation: 1,
               }}
             >
               <CardItem
@@ -142,7 +67,6 @@ const QuizScreen = ({
                   justifyContent: 'center',
                 }}
               >
-                {/* <Text>{item.ask}</Text> */}
                 <FlipCard
                   style={{
                     flex: 1,
@@ -154,10 +78,7 @@ const QuizScreen = ({
                   perspective={1000}
                   flipVertical
                   flip={false}
-                  clickable={true}
-                  onFlipEnd={isFlipEnd => {
-                    // console.log('isFlipEnd', isFlipEnd)
-                  }}
+                  clickable
                 >
                   <CardFace>
                     <Text>{item.ask}</Text>
@@ -184,39 +105,14 @@ const QuizScreen = ({
 
       <BottomView>
         <Bar
-          progress={cards && cardsList && (cardsList.length - cards.length) / cardsList.length}
+          progress={
+            cards &&
+            cardsList &&
+            (cardsList.length - cards.length) / cardsList.length
+          }
           width={200}
         />
       </BottomView>
-
-      {/* <BottomView>
-        <Bar progress={0.3} width={200} />
-      </BottomView>
-      <View
-        style={{
-          flexDirection: 'row',
-          flex: 0,
-          // position: 'absolute',
-          // bottom: 50,
-          // left: 0,
-          // right: 0,
-          justifyContent: 'space-between',
-          padding: 15,
-        }}
-      >
-        <Button iconLeft onPress={() => this._deckSwiper._root.swipeLeft()}>
-          <Icon name="arrow-back" />
-          <Text>ok</Text>
-        </Button>
-        <Button
-          danger
-          iconRight
-          onPress={() => this._deckSwiper._root.swipeRight()}
-        >
-          <Text>Repeat</Text>
-          <Icon name="arrow-forward" />
-        </Button>
-      </View> */}
     </Container>
   )
 }
@@ -255,22 +151,16 @@ const mapDispatchToProps = bindActionCreators({
 
 export default compose(
   withState('cards', 'setCards', ''),
-  withState('progress', 'calculateProgress', 0),
   withHandlers({
     changeAsk: ({ setCards }) => () => setCards(cards => cards),
-    changeProgress: ({ cards, cardsList, calculateProgress }) => () =>
-      calculateProgress(cards.length / cardsList.length),
   }),
   connect(
     mapStateToProps,
     mapDispatchToProps,
   ),
-  // withPropsOnChange(),
   lifecycle({
     componentDidMount() {
-      // const { id } = this.props.navigation.getParam('lesson')
       this.props.setCards(this.props.cardsList)
-      // console.log(this.props.cards)
     },
   }),
 )(QuizScreen)
