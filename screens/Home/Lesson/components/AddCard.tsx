a077fa321b9b4022afc0815f057482e2b8e438ce
yqbk <jksyrek@gmail.com>
2019-07-09 20:59:28 +0200

    Fix AddCard.

--- screens/Home/Lesson/components/AddCard.tsx
+++ screens/Home/Lesson/components/AddCard.tsx
@@ -1,46 +1,50 @@
 import React from 'react'
 import { Button, Text, Item, Input, Label, Card, CardItem } from 'native-base'
-
-import { ListView } from 'react-native'
-
+import { View } from 'react-native'
 import { connect } from 'react-redux'
-import { lifecycle, compose, withState, withHandlers } from 'recompose'
-
-import { View, FlatList } from 'react-native'
-import styled from 'styled-components'
+import { compose, withState } from 'recompose'
 
 import CardsActions from '../../../../containers/cards/actions'
 import { bindActionCreators } from '../../../../utils/reduxUtils'
 
-const AddCard = ({ lessonId, addCard, ask, setAsk, answer, setAnswer }) => {
-  return (
+const AddCard = ({
+  lessonId,
+  addCard,
+  ask,
+  setAsk,
+  answer,
+  setAnswer,
+  updateCards,
+}) => (
+  <View style={{ padding: 12 }}>
     <Card>
-      <View style={{ flex: 1 }}>
-        <Item floatingLabel={true} style={{ flex: 1 }}>
+      <CardItem>
+        <Item floatingLabel style={{ flex: 1 }}>
           <Label> Question </Label>
-          <Input onChangeText={setAsk} />
+          <Input onChangeText={setAsk} value={ask} />
         </Item>
-        <Item floatingLabel={true} style={{ flex: 1 }}>
+
+        <Item floatingLabel style={{ flex: 1 }}>
           <Label> Answer </Label>
-          <Input onChangeText={setAnswer} />
+          <Input onChangeText={setAnswer} value={answer} />
         </Item>
-      </View>
-      <CardItem footer={true}>
+
         <Button
           transparent={true}
-          onPress={() => addCard(lessonId, ask, answer)}
+          onPress={() => {
+            addCard(lessonId, ask, answer)
+            setAsk('')
+            setAnswer('')
+            updateCards()
+          }}
           disabled={!ask || !answer}
         >
           <Text> Add </Text>
         </Button>
       </CardItem>
     </Card>
-  )
-}
-
-const mapStateToProps = state => ({
-  //   cards: cardsSelector(state),
-})
+  </View>
+)
 
 const mapDispatchToProps = bindActionCreators({
   addCard: (lessonId, ask, answer) =>
@@ -49,13 +53,9 @@ const mapDispatchToProps = bindActionCreators({
 
 export default compose(
   connect(
-    mapStateToProps,
+    null,
     mapDispatchToProps,
   ),
   withState('ask', 'setAsk', ''),
   withState('answer', 'setAnswer', ''),
-  withHandlers({
-    changeAsk: ({ setAsk }) => () => setAsk(ask => ask),
-    changeAnswer: ({ setAnswer }) => () => setAnswer(answer => answer),
-  }),
 )(AddCard)
